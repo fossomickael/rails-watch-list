fe5d7d2143a1e0628b4dbc8df377058937f39bf6
Mickael FOSSO <fosso.mickael@gmail.com>
2021-05-20 14:51:38 +0200

    bookmarks add

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:create]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
