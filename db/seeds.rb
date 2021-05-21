f56d1a88501d2eb9a9fa3d5f6a48837d0222d043
Mickael FOSSO <fosso.mickael@gmail.com>
2021-05-21 16:38:11 +0200

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,18 @@
 # Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 # Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 # Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
 
 require 'open-uri'
 require 'json'
+prefix = "https://image.tmdb.org/t/p/original"
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 
 buffer = open(url).read
 movies = JSON.parse(buffer)["results"]
 
 movies.each do |movie|
-  Movie.create(title: movie["title"], overview:  movie["overview"], rating: movie["rating"])
+  Movie.create(title: movie["title"], overview: movie["overview"], rating: movie["rating"], poster_url: "#{prefix}#{movie["poster_path"]}")
 end
